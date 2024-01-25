73a6cefdf28fbdff4c77768abc3a608e6ebf2e2e
ImNoax <97470441+ImNoax@users.noreply.github.com>
2024-01-24 23:33:08 -0500

    transfer project

---
+++ site-web/src/components/Song.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useContext } from "react";
+import { ACTIONS } from "../reducers/reducer";
+
+import PlaylistContext from "../contexts/PlaylistContext";
+
+export default function Song({ song, index }) {
+  const { dispatch } = useContext(PlaylistContext);
+  const [liked, setLiked] = useState(song.liked);
+  // TODO : envoyer une demande de modification au serveur et mettre l'interface à jour.
+  const api = useContext(PlaylistContext).api;
+  const toggleLike = async () => { 
+    await api.updateSong(song.id)
+    setLiked(!liked);
+  };
+
+  // TODO : envoyer une action PLAY avec le bon index au reducer.
+  const playSong = () => {
+    if (index !== undefined) {
+      dispatch({ type: ACTIONS.PLAY, payload: { index: index - 1 } });
+    }
+  };
+  return (
+    <section
+      className="song-item flex-row"
+      onClick={() => {
+          playSong();
+      }}
+    >
+      {index !== undefined ? <span>{index}</span> : <></>}
+      {/*TODO : ajouter les bonnes informations de la chanson */}
+        <span>{song.name}</span>
+        <span>{song.artist}</span>
+        <span>{song.album}</span>
+        <span>{song.genre}</span>
+
+      {/*TODO : modifier le statut aimé seulement si index n'existe pas */}
+      <button
+        className={`${liked ? "fa" : "fa-regular"} fa-2x fa-heart`}
+        onClick={ () => {
+          if(!index){
+          toggleLike();
+          }
+        }
+        }
+      ></button>
+    </section>
+  );
+}
